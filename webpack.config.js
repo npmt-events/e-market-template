32d74a79d96a64cd69b6a42a129854cec0653809
Mohamed Kamal <53078974+mkchemist@users.noreply.github.com>
2020-02-19 11:04:09 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+  mode : 'development',
+  entry :{
+    'e-market-app' : './src/index.js'
+  },
+  output:{
+    filename : 'js/[name].js',
+    path : path.resolve(__dirname,'dist')
+  },
+  devServer: {
+    contentBase : path.join(__dirname,'dist'),
+    port : 9000
+  },
+  module:{
+    rules:[
+      {
+        test : /\.(c|sa|sc)ss$/,
+        use : [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins:[
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template : './public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename : 'css/[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
+}
